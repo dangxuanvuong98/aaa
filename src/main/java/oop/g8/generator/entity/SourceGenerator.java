4d2719acbf40e6a7f64ffbdc8706946398329e22
Dinh Hoang Nam <20162793@student.hust.edu.vn>
2018-12-24 10:50:27 +0700

    save work

--- src/main/java/oop/g8/generator/entity/SourceGenerator.java
+++ src/main/java/oop/g8/generator/entity/SourceGenerator.java
@@ -1,85 +1,85 @@
-//package oop.g8.generator.entity;
-//
-//import java.io.BufferedReader;
-//import java.io.FileInputStream;
-//import java.io.FileNotFoundException;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.util.ArrayList;
-//import java.util.Date;
-//import java.util.GregorianCalendar;
-//import java.util.List;
-//
-//import oop.g8.model.entity.Source;
-//
-//public class SourceGenerator {
-//	
-//	private static List<String> domain_list;
-//	
-//	/**
-//	 * 
-//	 * @param domain_file đường dẫn tới file chứa danh sách các domain
-//	 */
-//	public static void getData(String domain_file) {
-//				// Đọc danh sách domain
-//				domain_list = new ArrayList<String>();
-//				try (BufferedReader reader = new BufferedReader(new InputStreamReader(
-//						new FileInputStream(domain_file), "UTF8"))) {
-//					String name;
-//					while ((name = reader.readLine()) != null) {
-//						domain_list.add(name);
-//					}
-//				} catch (FileNotFoundException e) {
-//					System.out.println("Error: Missing filename: " + domain_file);
-//					e.printStackTrace();
-//				} catch (IOException e) {
-//					System.out.println("Error: Fail to read filename: "
-//							+ domain_file);
-//					e.printStackTrace();
-//				}
-//	}
-//	
-//	/**
-//	 * 
-//	 * @return một ngày/tháng/năm được sinh ngẫu nhiên
-//	 */
-//	public static Date randomDate() {
-//		int random_year = 1980 + (int) (Math.random() * 40);
-//		int random_month = 1 + (int) (Math.random() * 12);
-//		int random_day;
-//		if (random_month == 2) {
-//			if ((random_year % 4 == 0 && random_year % 100 != 0) || (random_year % 400 == 0)) {
-//				random_day = 1 + (int) (Math.random() * 29);
-//			}
-//			else {
-//				random_day = 1 + (int) (Math.random() * 28);
-//			}
-//		}
-//		else if ((random_month <= 7 && random_month % 2 == 1) || (random_month >= 8 && random_month % 2 == 0)){
-//			random_day = 1 + (int) (Math.random() * 31);
-//		}
-//		else {
-//			random_day = 1 + (int) (Math.random() * 30);
-//		}
-//		return (Date) new GregorianCalendar(random_year, random_month, random_day).getTime();
-//	}
-//	
-//	public static String randomLink() {
-//		if (domain_list.isEmpty()) {
-//			return "localhost";
-//		}
-//		int random_num = (int) (Math.random() * domain_list.size());
-//		return domain_list.get(random_num) + "/" + Integer.toString((int) (Math.random() * 1000000))+"/"+Integer.toString((int) (Math.random() * 1000000))+".html";
-//	}
-//	
-//	/**
-//	 * 
-//	 * @return thực thể Source sinh ngẫu nhiên
-//	 */
-//	public static Source generateSource() {
-//		Source source = new Source();
-//		source.setDate(randomDate());
-//		source.setLink(randomLink());
-//		return source;
-//	}
-//}
+package oop.g8.generator.entity;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import oop.g8.model.entity.Source;
+
+public class SourceGenerator {
+	
+	private static List<String> domain_list;
+	
+	/**
+	 * 
+	 * @param domain_file đường dẫn tới file chứa danh sách các domain
+	 */
+	public static void getData(String domain_file) {
+				// Đọc danh sách domain
+				domain_list = new ArrayList<String>();
+				try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+						new FileInputStream(domain_file), "UTF8"))) {
+					String name;
+					while ((name = reader.readLine()) != null) {
+						domain_list.add(name);
+					}
+				} catch (FileNotFoundException e) {
+					System.out.println("Error: Missing filename: " + domain_file);
+					e.printStackTrace();
+				} catch (IOException e) {
+					System.out.println("Error: Fail to read filename: "
+							+ domain_file);
+					e.printStackTrace();
+				}
+	}
+	
+	/**
+	 * 
+	 * @return một ngày/tháng/năm được sinh ngẫu nhiên
+	 */
+	public static Date randomDate() {
+		int random_year = 1980 + (int) (Math.random() * 40);
+		int random_month = 1 + (int) (Math.random() * 12);
+		int random_day;
+		if (random_month == 2) {
+			if ((random_year % 4 == 0 && random_year % 100 != 0) || (random_year % 400 == 0)) {
+				random_day = 1 + (int) (Math.random() * 29);
+			}
+			else {
+				random_day = 1 + (int) (Math.random() * 28);
+			}
+		}
+		else if ((random_month <= 7 && random_month % 2 == 1) || (random_month >= 8 && random_month % 2 == 0)){
+			random_day = 1 + (int) (Math.random() * 31);
+		}
+		else {
+			random_day = 1 + (int) (Math.random() * 30);
+		}
+		return (Date) new GregorianCalendar(random_year, random_month, random_day).getTime();
+	}
+	
+	public static String randomLink() {
+		if (domain_list.isEmpty()) {
+			return "localhost";
+		}
+		int random_num = (int) (Math.random() * domain_list.size());
+		return domain_list.get(random_num) + "/" + Integer.toString((int) (Math.random() * 1000000))+"/"+Integer.toString((int) (Math.random() * 1000000))+".html";
+	}
+	
+	/**
+	 * 
+	 * @return thực thể Source sinh ngẫu nhiên
+	 */
+	public static Source generateSource() {
+		Source source = new Source();
+		source.setDate(randomDate());
+		source.setLink(randomLink());
+		return source;
+	}
+}
--- src/main/java/oop/g8/service/InitDB.java
+++ src/main/java/oop/g8/service/InitDB.java
@@ -1,11 +1,32 @@
 package oop.g8.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import oop.g8.generator.entity.CountryGenerator;
+import oop.g8.generator.entity.EventGenerator;
+import oop.g8.generator.entity.LocationGenerator;
+import oop.g8.generator.entity.OrganizationGenerator;
+import oop.g8.generator.entity.PersonGenerator;
+import oop.g8.generator.entity.TimeGenerator;
+import oop.g8.generator.relation.country.C2CG;
+import oop.g8.model.entity.Country;
+import oop.g8.model.entity.Event;
+import oop.g8.model.entity.Location;
+import oop.g8.model.entity.Organization;
+import oop.g8.model.entity.Person;
+import oop.g8.model.entity.Time;
+import oop.g8.model.relation.country.C2C;
+
 @Service
 public class InitDB {
-	
+
+	@Autowired
+	private Wrap w;
+
 	@Autowired
 	private ScannerService scn;
 
@@ -13,10 +34,127 @@ public class InitDB {
 		System.out.println("---Sinh dữ liệu giả lập---");
 		System.out.print("->Số lượng thực thể: ");
 		int e = scn.getInputNum(100000000);
+
 		System.out.print("->Số lượng quan hệ: ");
 		int r = scn.getInputNum(100000000);
 		System.out.println("    Sinh " + e + " thực thể và " + r + " quan hệ");
-		// insert code here
 
+		int outterLoop1 = e / 6000;
+		int outterLoop2 = r / 6000;
+
+////////////////////////////////////////////////////////////////////////////
+		List<Person> pL1 = new ArrayList<>();
+		List<Person> pL = new ArrayList<>();
+		long s = System.currentTimeMillis();
+		for (int i = 0; i < outterLoop1; i++) {
+			for (int j = 0; j < 1000; j++) {
+				Person p = PersonGenerator.generatePerson();
+				pL.add(p);
+				pL1.add(p);
+			}
+			w.pr.saveAll(pL);
+			System.out.println("save Person " + i + "x1000 !");
+			pL.clear();
+		}
+		long f = System.currentTimeMillis();
+		System.out.println("time generate Person:" + (f - s));
+
+////////////////////////////////////////////////////////////////////////////
+		s = System.currentTimeMillis();
+		List<Organization> oL1 = new ArrayList<>();
+		List<Organization> oL = new ArrayList<>();
+		for (int i = 0; i < outterLoop1; i++) {
+			for (int j = 0; j < 1000; j++) {
+				Organization o = OrganizationGenerator.generateOrganization();
+				oL.add(o);
+				oL1.add(o);
+			}
+			w.or.saveAll(oL);
+			System.out.println("save Organization " + i + "x1000 !");
+			oL.clear();
+		}
+		f = System.currentTimeMillis();
+		System.out.println("time generate Country:" + (s - f));
+
+////////////////////////////////////////////////////////////////////////////		
+		s = System.currentTimeMillis();
+		List<Location> lL1 = new ArrayList<>();
+		List<Location> lL = new ArrayList<>();
+		for (int i = 0; i < outterLoop1; i++) {
+			for (int j = 0; j < 1000; j++) {
+				Location l = LocationGenerator.generateLocation();
+				lL.add(l);
+				lL1.add(l);
+			}
+			w.lr.saveAll(lL);
+			System.out.println("save Location " + i + "x1000 !");
+			lL.clear();
+		}
+		f = System.currentTimeMillis();
+		System.out.println("time generate Location:" + (s - f));
+
+////////////////////////////////////////////////////////////////////////////
+		s = System.currentTimeMillis();
+		List<Country> cL1 = new ArrayList<>();
+		List<Country> cL = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				Country c = CountryGenerator.generateCountry();
+				cL1.add(c);
+				cL.add(c);
+			}
+			w.cr.saveAll(cL);
+			System.out.println("save Country " + i + "x1000 !");
+			cL.clear();
+		}
+		f = System.currentTimeMillis();
+		System.out.println("time generate Location:" + (s - f));
+
+////////////////////////////////////////////////////////////////////////////	
+		s = System.currentTimeMillis();
+		List<Event> eL1 = new ArrayList<>();
+		List<Event> eL = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				Event e11 = EventGenerator.generateEvent();
+				eL.add(e11);
+				eL1.add(e11);
+			}
+			w.er.saveAll(eL);
+			System.out.println("save Event" + i + "x1000 !");
+			eL.clear();
+		}
+		f = System.currentTimeMillis();
+		System.out.println("time generate Event:" + (f - s));
+
+////////////////////////////////////////////////////////////////////////////
+		s = System.currentTimeMillis();
+		List<Time> tL1 = new ArrayList<>();
+		List<Time> tL = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				Time t = TimeGenerator.generateTime();
+				tL.add(t);
+				tL1.add(t);
+			}
+			w.tr.saveAll(tL);
+			System.out.println("save Time" + i + "x1000 !");
+			tL.clear();
+		}
+		f = System.currentTimeMillis();
+		System.out.println("time generate Event:" + (f - s));
+
+////////////////////////////////////////////////////////////////////////////
+		List<C2C> ccL = new ArrayList<>();
+		for (int i = 0; i < outterLoop1; i++) {
+			for (int j = 0; j < 1000; j++) {
+				C2C c2c = C2CG.generateC2C(cL1.get(index), cL1.get(index), relationName, link, date);
+				ccL.add(c2c);
+			}
+			w.ccr.saveAll(ccL);
+			System.out.println("save Time" + i + "x1000 !");
+			ccL.clear();
+		}
 	}
+
 }
